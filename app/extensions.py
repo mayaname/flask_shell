174e911dd3781590d6d5adf5cb058b6c2dc3a3ae
Wolfgang Miller <wmiller.geo@yahoo.com>
2025-05-02 08:24:55 -0400

    Update db instance w/ model class

--- app/extensions.py
+++ app/extensions.py
@@ -13,7 +13,11 @@ Revisions:
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 
-db = SQLAlchemy()
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
 login_manager = LoginManager()
